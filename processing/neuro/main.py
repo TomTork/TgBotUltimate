993f2f9bcc2b0497c458bb88d91cf7e4f083bfbb
tomtork <dima26483950@gmail.com>
2026-02-14 00:56:27 +0500

    Добавил поддержку маленькой нейронки, которая будет составлять json

--- processing/neuro/main.py
+++ processing/neuro/main.py
@@ -18,16 +18,38 @@ class Neuro:
         return response['response']
 
 
+class SimpleNeuro:
+    def __init__(self, model: str):
+        self.model = model
+        super().__init__()
+
+    async def ask(self, prompt: str):
+        response = await ollama.generate(
+            model=self.model,
+            prompt=prompt,
+            options={
+                "temperature": 1
+            }
+        )
+        return response['response']
+
+
 class Server:
     def __init__(self):
         self.app = FastAPI()
-        self.neuro = Neuro("qwen3-vl:8b")
+        self.neuro = Neuro("qwen3-vl:4b")
+        self.simple_neuro = SimpleNeuro("qwen3-vl:2b")
         self.setup()
 
     def setup(self):
         @self.app.get("/ask")
         async def ask(data):
             answer = await self.neuro.ask(data.prompt)
+            return {"response": answer}
+
+        @self.app.get("/parameters")
+        async def parameters(data):
+            answer = await self.simple_neuro.ask(data.prompt)
             return {"response": answer}
 
     def run(self):
